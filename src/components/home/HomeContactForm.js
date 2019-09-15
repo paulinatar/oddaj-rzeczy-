fb1cf85ffcac09f3116bcfcb8a2801213c89a50f
paulinatar <p.tarnawska@gmail.com>
2019-09-15 19:46:21 +0200

    contactform

--- src/components/home/HomeContactForm.js
+++ src/components/home/HomeContactForm.js
@@ -6,26 +6,45 @@ import '../../scss/styles/HomeContactForm.scss'
 
 class HomeContactForm extends Component{
     render() {
-        return(
+        return(<>
             <div className='container4' name ='HomeContactForm'>
                 <div className='leftFormImage'></div>
                 <div className='rightForm'>
                     <h2>Skontaktuj się z nami</h2>
                     <div className='decorations'></div>
+                    <div className='formDiv'>
                     <form className='contactForm'>
+                        <div className='inputName'>
                         <label>Wpisz swoje imię</label>
-                        <input type='name' className='inputName'/>
+                            <br/>
+                        <input type='name' className='inputName' placeholder='Krzysztof'/>
+                </div>
+                        <div className='inputEmail'>
                         <label>Wpisz swój email</label>
-                        <input type='email' className='inputName'/>
+                            <br/>
+                        <input type='email' className='inputName' placeholder='abc@xyz.pl'/>
+                        </div>
+                        <br/>
+                        <div className='newMessage'>
+
                         <label>Wpisz swoją wiadomośc</label>
-                        <textarea />
-                        <button>Wyślij</button>
+                            <br/>
+                        <textarea cols ='80' rows='4'  placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'/>
+                        </div>
+<br/>
+                        <button  value='send' type='submit'>Wyślij</button>
 
                     </form>
-
-
+                    </div>
                 </div>
-            </div>)
+
+            </div>
+            <div className='footerLab'>
+                <p>Copyrights by Coders Lab</p>
+                <div className='inLogo'></div>
+                <div className='fbLogo'></div>
+            </div>
+            </>)
 
 
 
--- src/components/home/HomeFoundationsList.js
+++ src/components/home/HomeFoundationsList.js
@@ -1,12 +1,39 @@
 import React,{Component} from "react";
-import {NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
 import '../../scss/styles/HomeFoundationsList.scss'
 
 
-class HomeFoundationsList extends Component{
-    render() {
-        return (<div className='container' name = 'HomeFoundationsList'>Foundations list</div>)
+class HomeFoundationsList extends Component {
+    state = {
+        foundationList: false,
+        ngosList: false,
+        localList: false
+    };
+
+
+render() {
+
+
+
+    return(
+
+
+            <div className='foundationsList'>
+                <h2>Komu pomagamy ?</h2>
+                <div className='decoration'></div>
+                <div className='buttons'>
+                    <button   onClick='pierwsza'>Fundacjom</button>
+                    <button onClick='druga'>Organizacjom pozarządowym</button>
+                    <button onClick='trezecia'>Lokalnym Zbiórkom</button>
+
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <div className='lists'>
+                    <lista>123</lista>
+                </div>
+            </div>)
+
+
     }
 }
-
 export default HomeFoundationsList
