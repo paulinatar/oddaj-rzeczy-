3407ee1dff8dbd678681965d2b2780c91881829a
paulinatar <p.tarnawska@gmail.com>
2019-10-02 12:20:26 +0200

    Updates

---
+++ precache-manifest.e2d9b68542140d60db40787337aff411.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a15d8ac5df0a2fd720933be12d73f8",
+    "url": "/oddaj-rzeczy-/index.html"
+  },
+  {
+    "revision": "962d21e26ccfdd869102",
+    "url": "/oddaj-rzeczy-/static/css/main.d3729009.chunk.css"
+  },
+  {
+    "revision": "abebe6d5454ee4b82dd9",
+    "url": "/oddaj-rzeczy-/static/js/2.a49be810.chunk.js"
+  },
+  {
+    "revision": "962d21e26ccfdd869102",
+    "url": "/oddaj-rzeczy-/static/js/main.87f38afc.chunk.js"
+  },
+  {
+    "revision": "a57b863c016af5319089",
+    "url": "/oddaj-rzeczy-/static/js/runtime~main.3c42799d.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/oddaj-rzeczy-/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/oddaj-rzeczy-/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/oddaj-rzeczy-/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/oddaj-rzeczy-/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/oddaj-rzeczy-/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/oddaj-rzeczy-/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/oddaj-rzeczy-/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/oddaj-rzeczy-/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/oddaj-rzeczy-/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/oddaj-rzeczy-/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/oddaj-rzeczy-/static/media/Signature.e54cfcfc.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oddaj-rzeczy-/static/media/logo.5d5d9eef.svg"
+  }
+]);
